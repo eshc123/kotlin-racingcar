0df62a84f27676fbdb264286655e5b46beb60b09
eshc123 <>
2024-11-16 13:03:52 +0900

    refactor: Calculator 매직넘버 상수로 선언

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -3,6 +3,9 @@ package calculator
 import calculator.Operator.Companion.operate
 
 object Calculator {
+    private const val FORMULA_DELIMITER = " "
+    private const val FORMULA_OPERATOR_INTERVAL = 2
+
     fun calculate(input: String?): Double {
         require(!input.isNullOrBlank()) { "계산할 수 없는 입력값입니다." }
 
@@ -13,14 +16,14 @@ object Calculator {
 
     private fun compute(formula: List<String>): Double {
         var result = formula.firstOrNull()?.toDouble() ?: throw NumberFormatException()
-        for (i in 1..formula.lastIndex step 2) {
+        for (i in 1..formula.lastIndex step FORMULA_OPERATOR_INTERVAL) {
             result = result.operate(formula[i + 1].toDouble(), Operator.from(formula[i]))
         }
         return result
     }
 
     private fun validateFormula(formula: String): List<String> {
-        formula.split(" ").let { list ->
+        formula.split(FORMULA_DELIMITER).let { list ->
             if (list.size % 2 == 0) throw IllegalArgumentException("올바른 수식이 아닙니다.")
             return list
         }
