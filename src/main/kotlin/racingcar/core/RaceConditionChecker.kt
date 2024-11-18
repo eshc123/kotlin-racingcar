c47cee941364782c87685854945e2b18267a5f6e
eshc123 <>
2024-11-19 00:52:19 +0900

    refactor: getRandomNumber 삭제

--- src/main/kotlin/racingcar/core/RaceConditionChecker.kt
+++ src/main/kotlin/racingcar/core/RaceConditionChecker.kt
@@ -5,9 +5,7 @@ object RaceConditionChecker {
     private const val MINIMUM_RANDOM_NUMBER = 0
     private const val MAXIMUM_RANDOM_NUMBER = 9
 
-    private val RANDOM_NUMBER_RANGE = MINIMUM_RANDOM_NUMBER..MAXIMUM_RANDOM_NUMBER
+    internal val RANDOM_NUMBER_RANGE = MINIMUM_RANDOM_NUMBER..MAXIMUM_RANDOM_NUMBER
 
-    internal fun getRandomNumber(): Int = RANDOM_NUMBER_RANGE.random()
-
-    fun isAdvancePossible(): Boolean = getRandomNumber() >= MINIMUM_ADVANCE_NUMBER
+    fun isAdvancePossible(): Boolean = RANDOM_NUMBER_RANGE.random() >= MINIMUM_ADVANCE_NUMBER
 }
--- src/test/kotlin/racingcar/RaceConditionCheckerTest.kt
+++ src/test/kotlin/racingcar/RaceConditionCheckerTest.kt
@@ -7,9 +7,7 @@ import racingcar.core.RaceConditionChecker
 class RaceConditionCheckerTest {
     @Test
     fun `0에서 9 사이의 무작위 숫자를 구할 수 있다`() {
-        repeat(1000) {
-            val actual = RaceConditionChecker.getRandomNumber()
-            assertTrue(actual in 0..9)
-        }
+        val actual = RaceConditionChecker.RANDOM_NUMBER_RANGE.random()
+        assertTrue(actual in 0..9)
     }
 }
