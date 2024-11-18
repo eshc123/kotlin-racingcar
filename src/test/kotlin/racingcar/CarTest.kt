481daa79d9d9ca3d9341cfafdb6d2471120ae459
eshc123 <>
2024-11-19 00:34:29 +0900

    refactor: CarTest - 전진하지 않는 케이스의 테스트 코드

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -11,4 +11,11 @@ class CarTest {
         val actual = car.moveIfPossible(true)
         assertTrue(actual.position == 2)
     }
+
+    @Test
+    fun `Car는 전진할 수 없는 조건일 때 전진하지 않는다`() {
+        val car = Car(position = 1)
+        val actual = car.moveIfPossible(false)
+        assertTrue(actual.position == 1)
+    }
 }
