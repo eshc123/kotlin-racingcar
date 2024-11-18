61ad99e9a409d4a88259a0dbe169d6a963188daa
eshc123 <>
2024-11-18 23:16:26 +0900

    refactor: randomNumberRange 상수 네이밍 컨벤션 적용

--- src/main/kotlin/racingcar/core/RaceConditionChecker.kt
+++ src/main/kotlin/racingcar/core/RaceConditionChecker.kt
@@ -5,9 +5,9 @@ object RaceConditionChecker {
     private const val MINIMUM_RANDOM_NUMBER = 0
     private const val MAXIMUM_RANDOM_NUMBER = 9
 
-    private val randomNumberRange = MINIMUM_RANDOM_NUMBER..MAXIMUM_RANDOM_NUMBER
+    private val RANDOM_NUMBER_RANGE = MINIMUM_RANDOM_NUMBER..MAXIMUM_RANDOM_NUMBER
 
-    internal fun getRandomNumber(): Int = randomNumberRange.random()
+    internal fun getRandomNumber(): Int = RANDOM_NUMBER_RANGE.random()
 
     fun isAdvancePossible(): Boolean = getRandomNumber() >= MINIMUM_ADVANCE_NUMBER
 }
